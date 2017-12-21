592d6c3e5864606c1d32fa9ddf03fea63875bcb0
dgarg <dgarg.hts@gmail.com>
2017-12-21 17:17:58 +0530

    Fixed contact page

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   default from: "no-reply@myapp.com"
 	def new_contact(contact)
 		@contact = contact
-    mail(to: 'dgarg.hts@gmail.com', subject: 'Develop new project')
+    mail(to: 'devsachin108@gmail.com', subject: 'Develop new project')
   end
 end
