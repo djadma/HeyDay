7580281b206bc9fe65027cc137d1bb303b59f94a
Dipak <jadamdipak@gmail.com>
2018-05-19 14:46:05 +0530

    Testing purpose commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
+    address: "smtp.gmail.com",
     port: 587,
-    domain: "www.heydaytechnologies.com",
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["EMAIL"],
-    password: ENV["EMAIL_PASSWORD"]
+    user_name: 'ashoks.heyday@gmail.com',
+    password: 'ashok1991'
   }
 end
