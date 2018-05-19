a6bf32e83fc0c2b29193b81830f4f9a8b84d6b72
ashok <ashoks.heyday@gmail.com>
2018-05-19 13:00:56 +0530

    Added design for heyday

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,12 @@
 class ContactsController < ApplicationController
-	def new
-		@contact = Contact.new
-	end
+  def new
+    @contact = Contact.new
+  end
 
-	def create
-		@contact = Contact.new(contact_params)
-		if @contact.save
-			redirect_to root_path
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path
       flash.now[:error] = nil
       flash.now[:notice] = 'Thank you for your message!'
       ContactMailer.new_contact(@contact).deliver_now
@@ -14,14 +14,14 @@ class ContactsController < ApplicationController
       flash.now[:error] = 'Cannot send message.'
       render :new
     end
-	end
+  end
 
-	# def message	
-	# 	ContactMailer.new_contact(@contact).deliver_now
-	# end
+  # def message 
+  #   ContactMailer.new_contact(@contact).deliver_now
+  # end
 
-	protected
-	def contact_params
-		params.require(:contact).permit(:name, :email, :message)
-	end
+  protected
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
+  end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   default from: "no-reply@myapp.com"
 	def new_contact(contact)
 		@contact = contact
-    mail(to: 'devsachin108@gmail.com', subject: 'Develop new project')
+    mail(to: 'ashoks.heyday@gmail.com', subject: 'Develop new project')
   end
 end
