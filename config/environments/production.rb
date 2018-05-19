a40621ceac62fd2a6094e4f17313a518580a1921
Dipak <jadamdipak@gmail.com>
2018-05-19 14:35:01 +0530

    Add env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,13 +87,15 @@ Rails.application.configure do
 
   # Action Mailer settings
   config.action_mailer.default_url_options = { host: "www.heydaytechnologies.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'ashoks.heyday@gmail.com',
-    password:             'ashok1991',
-    authentication:       'plain',
-    enable_starttls_auto: true
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "www.heydaytechnologies.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL"],
+    password: ENV["EMAIL_PASSWORD"]
   }
 end
